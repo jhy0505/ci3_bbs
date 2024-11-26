676a8acc59d7df1a1711bfdef67dea58df14fc21
jhy0505 <dev.chy8495@gmail.com>
2024-11-26 23:53:21 +0900

    회원가입 페이지 html 구조 수정 및 css 추가2

--- application/views/member/signUp.php
+++ application/views/member/signUp.php
@@ -1,78 +1,107 @@
-<h1>회원가입 페이지</h1>
+<div id="signUpBox">
+  <h1>회원가입</h1>
 
-<form action="progressSignUp" method="POST" id="signUpForm" class="signUpForm">
-  
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="id" class="input-label">아이디</label>
-      <div class="idInput">
-        <input type="text" name="member_id" id="member_id">
+  <form action="progressSignUp" class="signUpForm" id="signUpForm" method="POST">
+    
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="id">아이디</label>
+        <div class="idInput">
+          <input 
+            id="member_id"
+            name="member_id"
+            placeholder="아이디를 입력해주세요."
+            type="text">
+        </div>
       </div>
+      <label class="error" for="member_id"></label>
     </div>
-    <label class="error" for="member_id"></label>
-  </div>
-  
-  <br>
+    
+    <br>
 
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="pw" class="input-label">비밀번호</label>
-      <div class="pwInput">
-        <input type="password" name="member_pw" id="member_pw">
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="pw">비밀번호</label>
+        <div class="pwInput">
+          <input 
+            id="member_pw"
+            name="member_pw"
+            placeholder="비밀번호를 입력해주세요."
+            type="password">
+        </div>
       </div>
+      <label class="error" for="member_pw"></label>
     </div>
-    <label class="error" for="member_pw"></label>
-  </div>
-  
-  <br>
-  
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="name" class="input-label">이름</label>
-      <div class="nameInput">
-        <input type="text" name="member_name" id="member_name">
+    
+    <br>
+    
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="name">이름</label>
+        <div class="nameInput">
+          <input 
+            id="member_name"
+            name="member_name"
+            placeholder="이름을 입력해주세요."
+            type="text">
+        </div>
       </div>
+      <label class="error" for="member_name"></label>
     </div>
-    <label class="error" for="member_name"></label>
-  </div>
-  
-  <br>
-  
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="nickname" class="input-label">닉네임</label>
-      <div class="nicknameInput">
-        <input type="text" name="member_nickname" id="member_nickname">
+    
+    <br>
+    
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="nickname">닉네임</label>
+        <div class="nicknameInput">
+          <input
+            id="member_nickname"
+            name="member_nickname"
+            placeholder="닉네임을 입력해주세요."
+            type="text">
+        </div>
       </div>
+      <label class="error" for="member_nickname"></label>
     </div>
-    <label class="error" for="member_nickname"></label>
-  </div>
-  
-  <br>
-  
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="email" class="input-label">이메일</label>
-      <div class="emailInput">
-        <input type="text" name="member_email" id="member_email">
+    
+    <br>
+    
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="email">이메일</label>
+        <div class="emailInput">
+          <input 
+            id="member_email"
+            name="member_email"
+            placeholder="이메일을 입력해주세요."
+            type="text">
+        </div>
       </div>
+      <label class="error" for="member_email"></label>
     </div>
-    <label class="error" for="member_email"></label>
-  </div>
-  
-  <br>
-  
-  <div class="input-div">
-    <div class="input-div2">
-      <label for="mobile" class="input-label">휴대폰 번호</label>
-      <div class="mobileInput">
-        <input type="text" name="member_mobile" id="member_mobile">
+    
+    <br>
+    
+    <div class="input-div">
+      <div class="input-div2">
+        <label class="input-label" for="mobile">휴대폰 번호</label>
+        <div class="mobileInput">
+          <input 
+            id="member_mobile"
+            name="member_mobile"
+            placeholder="휴대폰 번호를 입력해주세요."
+            type="text">
+        </div>
       </div>
+      <label class="error" for="member_mobile"></label>
     </div>
-    <label class="error" for="member_mobile"></label>
-  </div>
-  
-  <br>
-  
-  <button>회원가입</button>
-</form>
+    
+    <br>
+    
+    <div class="btn-area">
+      <button>회원가입</button>
+    </div>
+
+  </form>
+</div>
